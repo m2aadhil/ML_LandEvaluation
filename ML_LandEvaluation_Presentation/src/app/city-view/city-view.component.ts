9a94103ae03bac2b2df74844da021a63ca54b8df
Aadhil Musthaq <musthaqaadhil@gmail.com>
2020-04-25 02:08:56 +0530

    fix lib check

--- ML_LandEvaluation_Presentation/src/app/city-view/city-view.component.ts
+++ ML_LandEvaluation_Presentation/src/app/city-view/city-view.component.ts
@@ -14,16 +14,21 @@ declare var jQuery: any;
 export class CityViewComponent implements OnInit, AfterViewInit {
 
   //@Input() stateName: string;
-  constructor(private httpService: HttpService) { }
+  address: string = "";
+  isLoadingCities: boolean = false;
   listYears = ["2019", "2020", "2021"];
+  listStates = ["California"];
+  state = "California";
   estimatedValue: string = "";
-  selectedYear: string;
-  longitude = 34.048927;
-  latitude = -111.093735;
+  selectedYear: string = "2020";
+  longitude = 36.47244485158913;
+  latitude = -119.79590870616853;
   listCounty = CountyCodeMapCA.sort((x, y) => x.code > y.code ? 1 : -1);
   listCities = [];
   location;
   county;
+  zoomLevel = 7;
+  setMarker: boolean = false;
 
   @ViewChild('map', { static: true }) mapElement: any;
   map: google.maps.Map;
@@ -36,12 +41,14 @@ export class CityViewComponent implements OnInit, AfterViewInit {
   ngAfterViewInit(): void {
     const mapProperties = {
       center: new google.maps.LatLng(this.longitude, this.latitude),
-      zoom: 10
+      zoom: this.zoomLevel
     };
     this.map = new google.maps.Map(this.mapElement.nativeElement, mapProperties);
     this.map.addListener('click', this.onClickMap);
 
   }
+
+  constructor(private httpService: HttpService) { }
 
   onClickMap = ($event) => {
     console.log($event)
@@ -61,15 +68,23 @@ export class CityViewComponent implements OnInit, AfterViewInit {
     geocoder.geocode({ 'location': latLng }, function (results) {
       if (results[0]) {
         console.log(results[0].formatted_address);
-        this.getPredictedPrice(results[0].formatted_address, results[0].geometry.location.lat(), results[0].geometry.location.lng());
+        if (this.setMarker) {
+          this.getPredictedPrice(results[0].formatted_address, results[0].geometry.location.lat(), results[0].geometry.location.lng());
+        }
+        this.setAddress(results[0].formatted_address);
       } else {
         console.log('No results found');
       }
     }.bind(this));
   }
 
+  setAddress = (address) => {
+    this.address = address.split(',')[0];
+  }
   cityChange($event): void {
-    let address = $event.CityName;
+    this.setMarker = false;
+    let address = this.listCities.find(i => i.CityCode == this.location).CityName;
+    this.zoomLevel = 15;
     let geocoder = new google.maps.Geocoder();
     geocoder.geocode({
       'address': address
@@ -77,35 +92,62 @@ export class CityViewComponent implements OnInit, AfterViewInit {
 
   }
 
-  valueChange($event): void {
-    let address = $event.name;
-    this.getCities($event.code);
+  countyChange($event): void {
+    this.setMarker = false;
+    let address = this.listCounty.find(i => i.code == this.county).name;
+    this.zoomLevel = 12;
+    this.getCities(this.county);
     let geocoder = new google.maps.Geocoder();
     geocoder.geocode({
       'address': address
     }, this.changeLocation);
+  }
+
+  onClickSubmit = () => {
+    if (this.address && this.location && this.county) {
+      this.setMarker = true;
+      this.zoomLevel = 18;
+      let address = this.address + ', ' + this.location + ', CA, USA';
+      let geocoder = new google.maps.Geocoder();
+      geocoder.geocode({
+        'address': address
+      }, this.changeLocation);
+    }
   }
 
   changeLocation = (results, status) => {
     if (status == google.maps.GeocoderStatus.OK) {
-      let Lat = results[0].geometry.location.lat();
-      let Lng = results[0].geometry.location.lng();
+      this.latitude = results[0].geometry.location.lat();
+      this.longitude = results[0].geometry.location.lng();
       var myOptions = {
-        zoom: 11,
-        center: new google.maps.LatLng(Lat, Lng)
+        zoom: this.zoomLevel,
+        center: new google.maps.LatLng(this.latitude, this.longitude)
       };
       this.map.setOptions(myOptions);
+      if (this.setMarker) {
+        if (this.marker) {
+          this.marker.setMap(null);
+        }
+        this.marker = new google.maps.Marker({
+          position: results[0].geometry.location,
+          map: this.map,
+          title: 'Hello World!'
+        });
+        this.getAddress(results[0].geometry.location);
+      }
     } else {
       alert("Something got wrong " + status);
     }
   }
 
   getCities = async (countyCode: string) => {
+    this.isLoadingCities = true;
     let url: string = environment.coreServiceUrl + "getcities/" + countyCode;
     this.listCities = [];
     await this.httpService.get(url).then((res: any[]) => {
+      this.isLoadingCities = false;
       if (res) {
-        this.listCities = res;
+        this.listCities = res.sort((x, y) => x.CityName > y.CityName ? 1 : -1);;
       }
     })
   }
@@ -113,7 +155,7 @@ export class CityViewComponent implements OnInit, AfterViewInit {
   getPredictedPrice = async (address: string, lat: string, lng: string) => {
     address = address.slice(0, address.length - 5);
     address = address.split('/,').join('/')
-    let url: string = environment.coreServiceUrl + "getpriceforloc/" + encodeURI(address) + '/' + lat + '/' + lng + '/' + this.selectedYear + '/' + this.location.CityCode;
+    let url: string = environment.coreServiceUrl + "getpriceforloc/" + encodeURI(address) + '/' + lat + '/' + lng + '/' + this.selectedYear + '/' + this.location;
     await this.httpService.get(url).then((res: any) => {
       if (res) {
         console.log(res);
